0f6517413de23a231d386e2a78051d9f66ca3163
Peter Sharp <peter@petersharp.org>
2016-03-05 22:17:43 +0000

    Added slider

---
+++ js/slick-index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.index-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 400,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
